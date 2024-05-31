db5be561253e8c804d92ea9cee26d345179c6d90
Rasolofondraibe <raobijaonanoah3@gmail.com>
2024-05-31 13:28:39 +0200

    ok

--- src/composant/Login.js
+++ src/composant/Login.js
@@ -19,7 +19,7 @@ const Login = ({setIsConnected,setCurrentComponent}) => {
 	setFormSubmitted(true)
 
     try {
-		const response = await fetch('http://localhost:2000/utilisateurs', {
+		const response = await fetch('https://etudiant-backend.vercel.app/utilisateurs', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
